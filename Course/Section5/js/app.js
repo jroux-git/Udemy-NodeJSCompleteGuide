e2eeb1923473295063c1894a469c3ef3d1015437
Jean Roux <jeanp.roux@gmail.com>
2019-03-25 19:45:15 +0200

    1. Added package.jsn \n 2. Added Express

--- Course/Section5/js/app.js
+++ Course/Section5/js/app.js
@@ -1,6 +1,18 @@
 const http = require("http");
-const routes = require("./routes");
+const express = require("express");
 
-const server = http.createServer(routes);
+const app = express();
+
+app.use((req, res, next)=>{
+    console.log("middleware");
+    next();
+});
+
+app.use((req, res, next)=>{
+    console.log("another middleware");
+    next();
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
