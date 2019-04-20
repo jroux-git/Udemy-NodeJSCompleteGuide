b006ff6fc61444efbd9d4a58da5ead1f42e620d9
Jean Roux <jeanp.roux@gmail.com>
2019-04-20 11:57:51 +0200

    1. Added to section 6: pug templating

--- Course/Section6/app.js
+++ Course/Section6/app.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+// Set global variable that will be used for view templates
+app.set('view engine','pug');
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -15,7 +18,8 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Page not found'});
 });
 
 app.listen(3000);
