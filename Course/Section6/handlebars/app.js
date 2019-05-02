9532f36746edc335d9d666f937c7dcc4d3d844f8
Jean Roux <jeanp.roux@gmail.com>
2019-05-02 09:27:16 +0200

    1. Split up Section6 into it main topics \n2. Started adding the handlebars code

--- Course/Section6/handlebars/app.js
+++ Course/Section6/handlebars/app.js
@@ -1,25 +1,27 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 const app = express();
 
 // Set global variable that will be used for view templates
-app.set('view engine','pug');
+app.engine("hbs", expressHbs());
+app.set("view engine", "hbs");
+app.set("views", "views");
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminData.routes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404', {pageTitle: 'Page not found'});
+  //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render("404", { pageTitle: "Page not found" });
 });
 
 app.listen(3000);
--- Course/Section6/handlebars/routes/shop.js
+++ Course/Section6/handlebars/routes/shop.js
@@ -1,15 +1,20 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const rootDir = require("../util/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('shop.js', adminData.products);
-  res.render('shop', {pageTitle: 'Shop', path: '/', prods: adminData.products});
+router.get("/", (req, res, next) => {
+  console.log("shop.js", adminData.products);
+  res.render("shop", {
+    pageTitle: "Shop",
+    path: "/",
+    prods: adminData.products,
+    hasProducts: adminData.products.length > 0
+  });
 });
 
 module.exports = router;
